5546148abc7579ffeaf0627b2a908eae9db03b4c
Neil Arora <neilarora2008@gmail.com>
2023-10-05 18:29:25 +0530

    update

--- learn/src/main/kotlin/variables.kt
+++ learn/src/main/kotlin/variables.kt
@@ -1,8 +1,8 @@
 val name = "Neil" //this variable cannot be modified
 var age = 15 //this variable can be modified
 val country : String = "India" //this is only needed if you wanna declare variable without assigning value
-val surname = "Arora"
+val surname = " Arora"
 fun main(){
     println(name)
-    println(name + " " + surname)
+    println(name + surname)
 }
