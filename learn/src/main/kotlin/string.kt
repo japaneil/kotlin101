ad9f9ce48515c05f543c06941cb266e001dfb1ca
Neil Arora <neilarora2008@gmail.com>
2023-10-06 18:46:09 +0530

    update

---
+++ learn/src/main/kotlin/string.kt
@@ -0,0 +1,10 @@
+val txt = "Hello World!"
+val txt2 = "Hello World!"
+fun main(){
+    println(txt[1])
+    println(txt.length)
+    println(txt.uppercase())
+    println(txt.lowercase())
+    println(txt.compareTo(txt2))
+    println("$txt $txt2") //the $ sign indicates towards a variable and replaces its value
+}
