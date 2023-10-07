aa31f9e4d12d382aaecece9bc39051d89d6d40ac
Neil Arora <neilarora2008@gmail.com>
2023-10-07 13:27:57 +0530

    update

--- learn/src/main/kotlin/breakContinue.kt
+++ learn/src/main/kotlin/breakContinue.kt
@@ -9,6 +9,19 @@ fun number(){
         num1++
     }
 }
+fun number1(){
+    num1 = 0
+    while (num1 < 51){
+        println(num1)
+        num1++
+        if (num1 == 21)
+            break
+    }
+    println("OVER")
+}
+
 fun main(){
     number()
+    println()
+    number1()
 }
