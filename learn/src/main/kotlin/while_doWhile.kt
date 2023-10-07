43edd211380e81ff9778fe0980b073b3c8ea0fff
Neil Arora <neilarora2008@gmail.com>
2023-10-07 12:47:03 +0530

    update

---
+++ learn/src/main/kotlin/while_doWhile.kt
@@ -0,0 +1,15 @@
+var num0 = 0
+fun main(){
+    while (num0 < 16){
+        println(num0)
+        num0++
+    }
+    doWhile()
+}
+
+fun doWhile(){
+    do {
+       println(num0)
+        num0++
+    } while(num0 < 26)
+}
